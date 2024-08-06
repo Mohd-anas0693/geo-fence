001c03b2463730f39c2032b1c71201bc7f65d031
Mohd-anas0693 <mohdanas0.693@gmail.com>
2024-08-06 17:09:19 +0530

    final code

--- src/components/Geofence/Geofence.jsx
+++ src/components/Geofence/Geofence.jsx
@@ -11,7 +11,6 @@ export const Geofence = () => {
 
   const [polygonPath, setPolygonPath] = useState();
   const [center, setCenter] = useState({ lat: 18.518056173723338, lng: 73.85410993103704 });
-  // const [locationData, setLocationData] = useState(null);
   const [selectedPlace, setSelectedPlace] = useState();
 
   const mapRef = useRef(null);
@@ -26,43 +25,6 @@ export const Geofence = () => {
     isFunctionalZoomEnabled: true
   };
 
-  // const _handleSearch = (locationData) => {
-  //   console.log("latlng: ", locationData)
-
-  //   const apiKey = 'b8568cb9afc64fad861a69edbddb2658';
-
-  //   const geometry = 'geometry_1000';
-  //   const place =  "ludhiana"
-
-  //   // const url = `https://nominatim.openstreetmap.org/reverse?lat=${locationData?.lat}&lon=${locationData?.lon}&polygon_geojson=1&format=json`
-  //   // const url = `https://nominatim.openstreetmap.org/search.php?q=st.%20john%27s&polygon_geojson=1&format=json` 
-  //   // const url = `https://nominatim.openstreetmap.org/search.php?q=ludhiana&polygon_geojson=1&format=json` 
-  //   const url = `https://nominatim.openstreetmap.org/search.php?q=${place}&polygon_geojson=1&format=json`; 
-
-  //   // const url = `https://api.geoapify.com/v1/boundaries/part-of?lon=${locationData?.lon}&lat=${locationData?.lat}&geometry=${geometry}&apiKey=${apiKey}`;
-  //   // https://api.geoapify.com/v1/boundaries/part-of?lon=12.789931425885811&lat=51.16772168907602&geometry=geometry_1000&apiKey=YOUR_API_KEY
-  //   fetch(url)
-  //     .then(response => {
-  //       if (!response.ok) {
-  //         throw new Error('Network response was not ok ' + response.statusText);
-  //       }
-  //       return response.json();
-  //     })
-  //     .then(data => {
-  //       console.log("api", data)
-  //       // const filteredLocationData = data?.features.filter((place)=> (place?.properties.place_id === locationData?.placeId))
-  //       // console.log(locationData  , ": location data")
-  //       // const filteredLocationData = data?.features.filter((place)=> (place?.properties.lat === locationData?.lat && place?.properties.lon))
-
-  //       // console.log("filterLocation", filteredLocationData);
-  //       // renderToMaps(filteredLocationData[0].geometry)
-  //       const coordinatesData = data[0].geojson;
-  //       console.log(coordinatesData)
-  //       renderToMaps(coordinatesData);
-  //     })
-  //     .catch(error => {
-  //       console.error('There has been a problem with your fetch operation:', error);
-  //     });
 
   const renderCoordinate = (paths) => {
     const bounds = new window.google.maps.LatLngBounds();
@@ -83,7 +45,6 @@ export const Geofence = () => {
       mapRef.current.fitBounds(bounds);
     }
   }
-  // const mapRef = useRef<google.maps.Map | null>(null);
 
 
 
@@ -139,19 +100,7 @@ export const Geofence = () => {
 
   return (
     <div>
-      {/* <AsyncTypeahead
-        className="TypeHead"
-        align="justify"
-        labelKey="display_name"
-        onSearch={(query) => _handleSearch(query)}
-        onChange={(query) => { renderToMaps(query) }}
-        options={options}
-        placeholder="Search city, Here"
-        renderMenuItemChildren={(option, props, index) => (
-          <div>
-            <span>{option.display_name}</span>
-          </div>
-        )} /> */}
+  
       <PlacesAutoComplete setSelectedPlace={(data) => { setSelectedPlace(data) }} />
       <GoogleMap
         mapContainerClassName="map-container"
--- src/components/PlacesAutoComplete/PlacesAutoComplete.jsx
+++ src/components/PlacesAutoComplete/PlacesAutoComplete.jsx
@@ -1,39 +1,4 @@
-// import usePlacesAutocomplete , {getGeocode, getLatLng} from "use-places-autocomplete";
-// import {
-// 	Combobox,
-// 	ComboboxInput,
-// 	ComboboxPopover,
-// 	ComboboxList,
-// 	ComboboxOption,
 
-// } from "@reach/combobox";
-
-// function  PlacesAutoComplete({setLocationData}){
-//   const {ready,value,setValue,suggestions:{status,data}, clearSuggestions} = usePlacesAutocomplete();
-//   const handleSelect = async (address) => {
-//     setValue(address, false);
-//     clearSuggestions();
-//     const results = await  getGeocode({address});
-//     console.log("results" , results[0].geometry.location.lat())
-//     setLocationData()
-//   }
-//   return <>
-//   <Combobox onSelect={handleSelect}>
-// x    <ComboboxInput 
-//     value={value}
-//      onChange={(e)=> setValue(e.target.value)} 
-//      disabled={!ready} 
-//      className="combox-input" 
-//      placeholder="Search an address" />
-//       <ComboboxPopover>
-//         <ComboboxList>
-//         {status === "OK" && data.map(({place_id,description}) => (<ComboboxOption key={place_id} value={description}/>))}
-//         </ComboboxList>
-//       </ComboboxPopover>
-//   </Combobox>
-//   </>
-// }
-// export default PlacesAutoComplete;
 import React, { useState, useCallback } from 'react'
 import "./PlacesAutoComplete.css"
 import debounce from 'lodash/debounce'
@@ -47,10 +12,11 @@ function PlacesAutoComplete({setSelectedPlace}) {
   
   const debouncedHandleChange = useCallback(debounce((value) => {
     console.log("value", value);
-    // const url = `https://nominatim.openstreetmap.org/search.php?q=st.%20john%27s&polygon_geojson=1&format=json`;
+    if(!value || value === "") return;
+    
     const url = `https://nominatim.openstreetmap.org/search.php?q=${value}&polygon_geojson=1&format=json`
     fetch(url)
-      .then(response => {
+      .then(response => { 
         if (!response.ok) {
           throw new Error('Network response was not ok ' + response.statusText);
 
